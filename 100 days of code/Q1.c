7172b20f21f6ab6e7b0b2eb1edb963c9b9b4def6
Arnav Aggarwal <103584859+arnav-aggarwal25@users.noreply.github.com>
2025-08-27 11:51:58 +0530

    main

---
+++ 100 days of code/Q1.c
@@ -0,0 +1,13 @@
+//Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main() {
+    int a,b,add;
+    
+    printf("Enter number 1 ");
+    scanf("%d",&a);
+    printf("Enter Numebr 2 ");
+    scanf("%d",&b);
+    add=a+b;
+    printf("%d",add);
+    return 0;
+}
